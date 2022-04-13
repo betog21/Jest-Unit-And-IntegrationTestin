aefcedb1b90f8b77a384e20b7952932b0b001e0e
Alberto González <alberto.gonzalez@distillery.com>
2022-04-13 11:15:37 -0500

    A failure test was implemented.

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,15 @@
 const TodoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../model/todo.model");
+
+TodoModel.create = jest.fn();
 
 describe("TodoController.createTodo", () => {
   it("Should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
+  });
+
+  it("Should call TodoModel.create", () => {
+    TodoController.createTodo();
+    expect(TodoModel.create).toBeCalled();
   });
 });

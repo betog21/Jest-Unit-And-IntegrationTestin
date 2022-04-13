a985f051f08e6c57f7fe885cddfaf31fbe3ee73b
Alberto González <alberto.gonzalez@distillery.com>
2022-04-13 16:59:56 -0500

    The integration test was added to the get Todo By Id.

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -3,6 +3,7 @@ const app = require("../../app");
 const newTodo = require("../mock-data/new-todo.json");
 
 const endpointURL = "/todos/";
+let firstTodo;
 
 describe(endpointURL, () => {
   test("GET " + endpointURL, async () => {
@@ -11,6 +12,14 @@ describe(endpointURL, () => {
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].title).toBeDefined();
     expect(response.body[0].done).toBeDefined();
+    firstTodo = response.body[0];
+  });
+
+  test("Get by Id " + endpointURL, async () => {
+    const response = await request(app).get(endpointURL + firstTodo._id);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe(firstTodo.title);
+    expect(response.body.done).toBe(firstTodo.done);
   });
 
   it("POST" + endpointURL, async () => {

4aa89412ab4e44268ef7158388146cc3fedf501f
Alberto González <alberto.gonzalez@distillery.com>
2022-04-13 12:34:16 -0500

    The todo route was created.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const todoRoutes = require("./routes/todo.routes");
+
+app.use("/todos", todoRoutes);
 
 app.get("/", (req, res) => {
   res.json("Hello.");

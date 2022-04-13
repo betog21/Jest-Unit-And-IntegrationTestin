469756dba3d8c3bdafd18e5717f2ce60c9204e98
Alberto González <alberto.gonzalez@distillery.com>
2022-04-13 13:40:41 -0500

    Error handling was implemented in the test.

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,6 +1,10 @@
 const TodoModel = require("../model/todo.model");
 
 exports.createTodo = async (req, res, next) => {
-  const createdMode = await TodoModel.create(req.body);
-  res.status(201).json(createdMode);
+  try {
+    const createdMode = await TodoModel.create(req.body);
+    res.status(201).json(createdMode);
+  } catch (error) {
+    next(error);
+  }
 };
